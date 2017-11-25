df9cb3938943be98ee0be3b38d51fbf4edffff9e
tsennott <timothy.sennott@gmail.com>
2017-11-24 23:21:08 -0800

    upload to gif working locally

--- Django/myproject/myproject/myapp/urls.py
+++ Django/myproject/myproject/myapp/urls.py
@@ -4,6 +4,8 @@ from myproject.myapp.views import upload
 from myproject.myapp.views import new_guid
 from myproject.myapp.views import gallery
 from myproject.myapp.views import info
+from myproject.myapp.views import gif
+
 
 urlpatterns = [
     url(r'^$', new_guid, name='new_guid'),
@@ -11,5 +13,5 @@ urlpatterns = [
     url(r'^upload/(?P<guid_id>[0-9]+)$', upload, name='upload'),
     url(r'^gallery/(?P<guid_id>[0-9]+)$', gallery, name='gallery'),
     url(r'^info/(?P<guid_id>[0-9]+)$', info, name='info'),
-    url(r'^gallery/', gallery, name='gallery')
+    url(r'^gif/(?P<guid_id>[0-9]+)$', gif, name='gif'),
 ]
--- Django/myproject/myproject/myapp/views.py
+++ Django/myproject/myproject/myapp/views.py
@@ -10,8 +10,10 @@ from myproject.myapp.models import Document
 from myproject.myapp.models import User
 from myproject.myapp.forms import DocumentForm
 
-from myproject.myapp.pointillism import pointillize
+from myproject.myapp.pointillism import pointillize, pointillizeStack
 from PIL import Image
+import imageio
+
 import io
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
@@ -90,3 +92,62 @@ def gallery(request, guid_id):
 def info(request, guid_id):
 
     return render(request, 'info.html', {'guid_id': guid_id})
+
+
+@csrf_exempt
+def gif(request, guid_id):
+
+    user = get_object_or_404(User, pk=guid_id)
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            orig_file = request.FILES['docfile']
+            orig_image = Image.open(orig_file)
+            point = pointillizeStack(image=orig_image, reduce_factor=2,
+                                     border=0, queue=True)
+            point.crop_Y([1920, 1080], resize=True)
+            point.plotRecPoints(n=40, multiplier=1, fill=True)
+            point.plotRandomPointsComplexity(n=2e4, constant=0.01, power=1.3)
+            multipliers = [5, 4.5, 4, 3.5, 3, 2.6, 2.3, 2, 1.75,
+                           1.5, 1.25, 1.1, 1, 1]
+            multipliers += multipliers[::-1]
+            point.build_multipliers(multipliers)
+            point.save_gif('temp/' + guid_id + '_pointqueue.gif', 0.1)
+            new_stringIO = io.BytesIO()
+            with open('temp/' + guid_id + '_pointqueue.gif', "rb") as imageFile:
+                f = imageFile.read()
+                b = bytearray(f)
+            new_stringIO.write(b)
+            new_file = InMemoryUploadedFile(new_stringIO,
+                                            u"docfile",  # change this?
+                                            (orig_file.name.split('.')[0] +
+                                             ' pointillized.gif'),
+                                            'image/gif',
+                                            None,
+                                            None)
+            newdoc = user.document_set.create(docfile=new_file)
+            newdoc.save()
+            origdoc = user.document_set.create(docfile=orig_file)
+            origdoc.save()
+
+            # Redirect to the document upload page after POST
+            return HttpResponseRedirect(reverse('gif',
+                                                kwargs={'guid_id': user.pk}))
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the upload page
+    all_documents = user.document_set.order_by("-id")
+    documents = []
+    for document in all_documents:
+            if (document.docfile.name[-16:] == 'pointillized.gif'):
+                documents.append(document)
+
+    # Render upload page with the documents and the form
+    return render(
+        request,
+        'upload_to_gif.html',
+        {'documents': documents, 'form': form, 'guid_id': user.pk}
+    )
+
