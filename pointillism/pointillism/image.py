2fac15b91c219c2e766b9ea4ab950773b71e25eb
tsennott <timothy.sennott@gmail.com>
2018-07-09 20:47:36 -0700

    trying getpixel() directly

--- pointillism/pointillism/image.py
+++ pointillism/pointillism/image.py
@@ -615,7 +615,7 @@ class image(object):
     def _testProbability(self, loc, use_coverage):
         if use_coverage:
             location = (loc[0] + self.border, loc[1] + self.border)
-            probability = max(1.0 - self.out_coverage.getdata().getpixel(location) / 255.0, 0.0)
+            probability = max(1 - self.out_coverage.getpixel(location) / 255, 0)
 
         else:
             probability = 1
