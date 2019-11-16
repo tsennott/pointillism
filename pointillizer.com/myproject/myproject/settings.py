7be91b49a17c1f0a04a5efb6dd4d91e7265e9a37
Tim Sennott <timothy.sennott@gmail.com>
2019-11-16 16:33:41 -0500

    forgot this was public, revoked IAM role

--- pointillizer.com/myproject/myproject/settings.py
+++ pointillizer.com/myproject/myproject/settings.py
@@ -17,6 +17,8 @@ import os
 
 # import secrets from a secret file
 from secrets import SECRET_KEY, SECRET_RDS_HOST, SECRET_ALLOWED_HOSTS
+from secrets import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from secrets import AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,11 +144,6 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-
-AWS_STORAGE_BUCKET_NAME = 'myproject-s3-bucket'
-AWS_S3_REGION_NAME = 'us-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAIPR4LBZGXTAR5IGQ'
-AWS_SECRET_ACCESS_KEY = 'vO6R43l2D9tDITT6qDQ6cyu5CcPdOOQBJvUl6BQM'
 
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
